0dd166a1557a359d52f4d960089b115da458775c
yh08037 <yh08037@gmail.com>
2021-06-11 23:23:35 +0900

    baseline model

--- common/functions.py
+++ common/functions.py
@@ -1,42 +1,61 @@
-import sys
-sys.path.append('..')
-from common.np import *
+# coding: utf-8
+import numpy as np
+
+
+def identity_function(x):
+    return x
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
 
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))    
 
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+    
 
 def relu(x):
     return np.maximum(0, x)
 
 
+def relu_grad(x):
+    grad = np.zeros(x)
+    grad[x>=0] = 1
+    return grad
+    
+
 def softmax(x):
     if x.ndim == 2:
-        # overflow를 막기 위해 입력값 중 
-        # 최대값을 빼준다. >> 밑러닝-1, 3.5.2 참고
-        x = x - x.max(axis=1, keepdims=True)
-        x = np.exp(x)
-        x /= x.sum(axis=1, keepdims=True)
-    elif x.ndim == 1:
-        x = x - np.max(x)
-        x = np.exp(x) / np.sum(np.exp(x))
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T 
 
-    return x
+    x = x - np.max(x) # 오버플로 대책
+    return np.exp(x) / np.sum(np.exp(x))
+
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
 
 
 def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
-
-    # 정답 데이터가 원핫 벡터일 경우 정답 레이블 인덱스로 변환
+        
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
     if t.size == y.size:
         t = t.argmax(axis=1)
-    
+             
     batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
-    cross_entropy = np.log(y[np.arange(batch_size), t] + 1e-7)
-    loss = -np.sum(cross_entropy) / batch_size
-    
-    return loss
+
+def softmax_loss(X, t):
+    y = softmax(X)
+    return cross_entropy_error(y, t)
